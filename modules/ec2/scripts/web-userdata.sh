6b3931648ac12c65bb2c866b4db4f431e64ec1af
Nisarg153 <nisarg150301@gmail.com>
2025-06-10 20:29:57 -0400

    user data script

---
+++ modules/ec2/scripts/web-userdata.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo yum update -y
+
+cd /home/ec2-user
+git clone https://github.com/Nisarg153/Multi-Environment-Deployment.git
+cd Multi-Environment-Deployment/app-code
+
+sudo npm install
+sudo npm install aws-sdk express body-parser dotenv
+sudo npm install -g pm2
+sudo chown -R ec2-user:ec2-user /home/ec2-user/Multi-Environment-Deployment
+
+pm2 start app.js --name app
+pm2 startup systemd -u ec2-user --hp /home/ec2-user | tail -n 1 | sh
+pm2 save
